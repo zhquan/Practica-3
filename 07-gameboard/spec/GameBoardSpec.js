a6d54758e3f136633721d01141ef9ea5d673334d
Quan Zhou <zhquan7@gmail.com>
2013-10-09 16:32:12 +0200

    archivo original 07-08

---
+++ 07-gameboard/spec/GameBoardSpec.js
@@ -0,0 +1,58 @@
+/*
+
+  En el anterior prototipo (06-player), el objeto Game permite
+  gestionar una colección de tableros (boards). Los tres campos de
+  estrellas, la pantalla de inicio, y el sprite de la nave del
+  jugador, se añaden como tableros independientes para que Game pueda
+  ejecutar sus métodos step() y draw() periódicamente desde su método
+  loop(). Sin embargo los objetos que muestran los tableros no pueden
+  interaccionar entre sí. Aunque se añadiesen nuevos tableros para los
+  misiles y para los enemigos, resulta difícil con esta arquitectura
+  pensar en cómo podría por ejemplo detectarse la colisión de una nave
+  enemiga con la nave del jugador, o cómo podría detectarse si un
+  misil disparado por la nave del usuario ha colisionado con una nave
+  enemiga.
+
+
+  Requisitos:
+
+  Este es precisamente el requisito que se ha identificado para este
+  prototipo: diseñar e implementar un mecanismo que permita gestionar
+  la interacción entre los elementos del juego. Para ello se diseñará
+  la clase GameBoard. Piensa en esta clase como un tablero de un juego
+  de mesa, sobre el que se disponen los elementos del juego (fichas,
+  cartas, etc.). En Alien Invasion los elementos del juego serán las
+  naves enemigas, la nave del jugador y los misiles. Para el objeto
+  Game, GameBoard será un board más, por lo que deberá ofrecer los
+  métodos step() y draw(), siendo responsable de mostrar todos los
+  objetos que contenga cuando Game llame a estos métodos.
+
+  Este prototipo no añade funcionalidad nueva a la que ofrecía el
+  prototipo 06.
+
+
+  Especificación: GameBoard debe
+
+  - mantener una colección a la que se pueden añadir y de la que se
+    pueden eliminar sprites como nave enemiga, misil, nave del
+    jugador, explosión, etc.
+
+  - interacción con Game: cuando Game llame a los métodos step() y
+    draw() de un GameBoard que haya sido añadido como un board a Game,
+    GameBoard debe ocuparse de que se ejecuten los métodos step() y
+    draw() de todos los objetos que contenga
+
+  - debe ofrecer la posibilidad de detectar la colisión entre
+    objetos. Un objeto sprite almacenado en GameBoard debe poder
+    detectar si ha colisionado con otro objeto del mismo
+    GameBoard. Los misiles disparados por la nave del jugador deberán
+    poder detectar gracias a esta funcionalidad ofrecida por GameBoard
+    cuándo han colisionado con una nave enemiga; una nave enemiga debe
+    poder detectar si ha colisionado con la nave del jugador; un misil
+    disparado por la nave enemiga debe poder detectar si ha
+    colisionado con la nave del jugador. Para ello es necesario que se
+    pueda identificar de qué tipo es cada objeto sprite almacenado en
+    el tablero de juegos, pues cada objeto sólo quiere comprobar si ha
+    colisionado con objetos de cierto tipo, no con todos los objetos.
+
+*/
---
+++ 08-missiles/spec/PlayerMissileSpec.js
@@ -0,0 +1,29 @@
+/*
+
+  Requisitos: 
+
+  La nave del usuario disparará 2 misiles si está pulsada la tecla de
+  espacio y ha pasado el tiempo de recarga del arma.
+
+  El arma tendrá un tiempo de recarga de 0,25s, no pudiéndose enviar
+  dos nuevos misiles antes de que pasen 0,25s desde que se enviaron
+  los anteriores
+
+
+
+  Especificación:
+
+  - Hay que añadir a la variable sprites la especificación del sprite
+    missile
+
+  - Cada vez que el usuario presione la tecla de espacio se añadirán
+    misiles al tablero de juego en la posición en la que esté la nave
+    del usuario. En el código de la clase PlayerSip es donde tienen
+    que añadirse los misiles
+
+  - La clase PlayerMissile es la que implementa los misiles. Es
+    importante que la creación de los misiles sea poco costosa pues va
+    a haber muchos disparos, para lo cual se declararán los métodos de
+    la clase en el prototipo
+
+*/
